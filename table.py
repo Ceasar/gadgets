dc2d4eb5d218d7da9e1d9d8b9d704ceccecbf759
Ceasar Bautista <cbautista2010@gmail.com>
2011-08-29 00:44:25 -0400

    Rewrote table.py to use a CSV to store data. table.py can also be used as an abstracted version of a CSV, as opposed to importing the csv module and learnining how to use it.

--- table.py
+++ table.py
@@ -1,49 +1,104 @@
 '''A SQL table representation.'''
+import csv
+import os
+import shutil
 
-from collections import namedtuple
+class Table(object):
+    '''A CSV backed SQL table.'''
+    @apply
+    def fieldnames():
+        def fget(self):
+            with open(self.filename) as f:
+                return csv.DictReader(f).fieldnames
 
-
-class Table(list):
-    '''Make a table with specified columns and rows.'''
-    def __init__(self, name='Row', columns=(), rows=[]):
-        self.name = name
-        self.columns = tuple(columns)
-        self.Row = namedtuple(name, columns)
-        super(Table, self).__init__([self.Row(*row) for row in rows])
+        def fset(self, fieldnames):
+            with open(self.filename, 'w') as f:
+                dr = csv.reader(f)
+                dw = csv.DictWriter(f, fieldnames=fieldnames)
+                dw.writerow(dict((field, field) for field in fieldnames))
+                for row in self:
+                    dw.writerow(row)
+        return property(**locals())
     
-    def select(self, columns):
-        '''Select all values from given columns.'''
-        if hasattr(columns, '__iter__'):
-            columns = [columns]
-        nrow = namedtuple('Row', columns)
-        for row in self:
-            yield nrow([getattr(row, column) for column in columns])
+    def __init__(self, filename, fieldnames=None):
+        self.filename = filename
+        if not fieldnames is None:
+            self.fieldnames = fieldnames
 
-    def where(self, condition):
-        '''Select all vectors matching the conditions.'''
-        for row in self:
-            if condition(row):
+    def select(self, columns=None, where=None, as_dict=False):
+        '''Select all values from given columns.
+
+        Leave columns as None in order to select all columns.
+        Leave where as None in order to select all rows.
+
+        where should take a dictionary representing the row argument as its
+        sole parameter and return a boolean.'''
+        columns = self.fieldnames if columns is None else columns
+        where = (lambda row: True) if where is None else where
+        for row in self.named_iter():
+            if where(row):
+                #this is suboptimal, but a bit more readable
+                if as_dict:
+                    yield [{column: row[column]} for column in columns]
+                else:
+                    yield [row[column] for column in columns]
+
+    def update(self, func, as_dict=False):
+        '''Apply func to each row.
+
+        Note: if a row should be deleted, return None rather than an empty
+        list to delete the row.'''
+        #Unfortunately, one cannot just change a line.
+        #Instead, one must completely rewrite the file with each change.
+        copy = CSV(self.filename + '~')
+        with open(self.filename, 'r') as f:
+            if as_dict:
+                reader = csv.DictReader(f)
+                copy.fieldnames = reader.fieldnames
+            else:
+                reader = csv.reader(f)
+                copy.fieldnames = reader.next()
+            for row in reader:
+                new = func(row)
+                #if new is None, don't write, effectively enabling deletion
+                if new: copy.append(new)
+        shutil.copyfile(copy.filename, self.filename)
+        os.remove(copy.filename)
+
+    def append(self, row):
+        '''Append a row to the end of the file.'''
+        with open(self.filename, 'a') as f:
+            if isinstance(row, dict):
+                writer = csv.DictWriter(f)
+            else:
+                writer = csv.writer(f)
+            writer.writerow(row)
+
+    def extend(self, rows):
+        '''Append mutliple rows to the end of the file.'''
+        with open(self.filename, 'a') as f:
+            dwriter = csv.DictWriter(f)
+            writer = csv.writer(f)
+            for row in rows:
+                if isinstance(row, dict):
+                    dwriter.writerow(row)
+                else:
+                    writer.writerow(row)
+
+    def __iter__(self):
+        #In order to be consistent with named_iter, the header is removed.
+        with open(self.filename) as f:
+            reader = csv.reader(f)
+            header = reader.next()
+            for row in reader:
                 yield row
 
-    def write(self, filename):
-        '''Writes the table as a csv to a file.'''
-        with open(filename, 'w') as f:
-            f.write(', '.join(self.columns))
-            map(lambda row: f.write(', '.join([str(field) for field in row])), self.rows)
-    
-    def append(self, item):
-        super(Table, self).append(self.Row(item))
-
-    def __setitem__(self, i, y):
-        raise NotImplementedError
+    def named_iter(self):
+        '''Iterate through the labeled rows.'''
+        with open(self.filename, 'rb') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                yield row
 
     def __str__(self):
-        return self.name
-
-class CSV(Table):
-    '''A representation of a CSV.'''
-    def __init__(self, filename, name='Row'):
-        with open(filename) as f:
-            columns = [col.strip() for col in f.readline().split(',')]
-            rows = [line.strip().split(',') for line in f]
-            super(CSV, self).__init__(name, columns, rows)
+        return self.filename
