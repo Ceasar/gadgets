666ed5a8ff1573561793da64474886c6bcfb6652
Ceasar Bautista <cbautista2010@gmail.com>
2011-09-12 14:41:39 -0400

    Renamed Space to Matrix, and created a new object, Space, which uses a dictionary with point (represented by tuples) -> value.

--- space.py
+++ space.py
@@ -1,22 +1,38 @@
+from itertools import product
 
 
-class Space(list):
+class Matrix(list):
   '''A null matrix of specified dimensions.
 
-  s = Space(2, 2)
-  >>> s
+  >>> m = Matrix((2, 2))
+  >>> m
   [[0, 0], [0, 0]]
-  >>> s[0][1]
+  >>> m[0][1]
   0
-  >>> s = Space(2, 2, 2)
-  >>> s
-  [[[0, 0], [0, 0]], [[0, 0], [0, 0]]]
-  >>> s[0][1][0]
-  0
+  >>> m = Matrix((2, 2, 2), 1)
+  >>> m
+  [[[1, 1], [1, 1]], [[1, 1], [1, 1]]]
+  >>> m[0][1][0]
+  1
   '''
-  def __init__(self, *dimensions):
+  def __init__(self, dimensions, default=0):
+    print dimensions
     if len(dimensions) > 1:
-      super(Space, self).__init__([Space(*dimensions[1:]) for d
-        in xrange(dimensions[0])])
+      super(Matrix, self).__init__([Matrix(dimensions[1:], default)
+        for d in xrange(dimensions[0])])
     else:
-      super(Space, self).__init__([0] * dimensions[0])
+      super(Matrix, self).__init__([default] * dimensions[0])
+
+class Space(dict):
+  '''A null space of specified dimensions.
+
+  >>> s= Space((2, 2))
+  >>> s
+  {(0, 1): 0, (1, 0): 0, (0, 0): 0, (1, 1): 0}
+  >>> s = Space((2, 2, 2), [])
+  >>> s
+  {(0, 1, 1): [], (1, 1, 0): [], (1, 0, 0): [], (0, 0, 1): [], (1, 0, 1): [], (0, 0, 0): [], (0, 1, 0): [], (1, 1, 1): []}
+  '''
+  def __init__(self, dimensions, default=0):
+    for point in product(*[range(d) for d in dimensions]):
+      self[point] = default
