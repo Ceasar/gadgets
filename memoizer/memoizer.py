65c73f6b17c6e893b50078a7f9dee7984980fdb9
Ceasar Bautista <cbautista2010@gmail.com>
2011-11-05 17:41:42 -0400

    Changed strategy to not default to None, and removed outdated documentation.

--- memoizer/memoizer.py
+++ memoizer/memoizer.py
@@ -2,12 +2,20 @@
 
 
 class Memoizer(object):
-  """A memoizer object. Speeds up functions at the cost of memory.
-
-  If no strategy is provided the memoizer will remember everything.
   """
-  def __init__(self, strategy=None):
+  A memoizer object.
+
+  Takes a strategy and applies it to a function.
+  """
+  def __init__(self, strategy):
     self.strategy = strategy
 
   def __call__(self, func):
     return self.strategy(func)
+
+"""
+def memoize(strategy):
+  def wrap(func):
+    return strategy(func)
+  return wrap
+"""
