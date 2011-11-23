512a52eca10644eb9f67214c034bb5f1e547b20e
Ceasar Bautista <cbautista2010@gmail.com>
2011-11-23 15:26:40 -0500

    Removed ceasarset. Use for x in y: return y instead.

--- ceasarset.py
+++
@@ -1,8 +0,0 @@
-
-class CeasarSet(set):
-  """A set object supporting a peek method for selecting an abritrary element."""
-
-  def peek(self):
-    """Select an arbritrary element from the set."""
-    for x in self:
-      return x
