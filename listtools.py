3e72df1fc3f377b75188c1abdf60fe35dfddd925
Ceasar Bautista <cbautista2010@gmail.com>
2011-11-23 15:26:03 -0500

    merged enumerators into listtools

--- listtools.py
+++ listtools.py
@@ -1,8 +1,11 @@
 """A collection of functions for manipulating iterables."""
+from itertools import izip
 
+
+#NOTE: Generally itertools.chain is a better solution than either of the below
 flatten = lambda *x: sum(x, [])
-
 splice = lambda x, i, y: x[:i] + y + x[i:] 
+
 
 def divide(list_, k):
   '''Slice a list into pieces of length-k.
@@ -17,6 +20,21 @@ def divide(list_, k):
     yield list_[i:i+k]
 
 
+def rr_enumerate(iterable):
+  '''Enumerate a reversed iterable.'''
+  return izip(reversed(xrange(len(iterable))), reversed(iterable))
+
+
+def fr_enumerate(iterable):
+  '''Enumerate a reversed iterable.'''
+  return izip(xrange(len(iterable)), reversed(iterable))
+
+
+def rf_enumerate(iterable):
+  '''Enumerate a reversed iterable.'''
+  return izip(reversed(xrange(len(iterable))), iterable)
+
+
 if __name__ == "__main__":
-	import doctest
-	doctest.testmod()
+  import doctest
+  doctest.testmod()
