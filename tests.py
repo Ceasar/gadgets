1b976f0933b6c914fc12457cfdbbe6cd47192855
Ceasar Bautista <cbautista2010@gmail.com>
2011-11-23 03:01:26 -0500

    Changed some tests.

--- tests.py
+++ tests.py
@@ -2,6 +2,36 @@ import unittest
 
 import abstract
 
+class testQueue(unittest.TestCase):
+  def setUp(self):
+    self.queue = abstract.queue()
+
+  def testPushPop(self):
+    self.queue.push(0)
+    self.assertEqual(self.queue.pop(), 0)
+
+    self.queue.push(0)
+    self.queue.push(1)
+    self.assertEqual(self.queue.pop(), 0)
+    self.assertEqual(self.queue.pop(), 1)
+
+  def testLen(self):
+    self.assertEqual(len(self.queue), 0)
+
+    self.queue.push(0)
+    self.queue.push(1)
+    self.assertEqual(len(self.queue), 2)
+    self.queue.pop()
+    self.queue.pop()
+    self.assertEqual(len(self.queue), 0)
+
+  def testIter(self):
+    self.queue.push(0)
+    self.queue.push(1)
+    for item in self.queue:
+    	pass
+    self.assertEqual(len(self.queue), 0)
+
 class testStack(unittest.TestCase):
   def setUp(self):
     self.stack = abstract.stack()
