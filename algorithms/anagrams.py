07b1ca851489426de09cf0928bbc0952f246f3d7
Ceasar Bautista <cbautista2010@gmail.com>
2012-02-04 22:52:55 -0500

    Updated at per Alexey's suggestion.

--- algorithms/anagrams.py
+++ algorithms/anagrams.py
@@ -1,40 +1,4 @@
-from collections import defaultdict
-
-PRIMES = {
-    'a': 2,
-    'b': 3,
-    'c': 5,
-    'd': 7,
-    'e': 11,
-    'f': 13,
-    'g': 17,
-    'h': 19,
-    'i': 23,
-    'j': 29,
-    'k': 31,
-    'l': 37,
-    'm': 41,
-    'n': 43,
-    'o': 47,
-    'p': 53,
-    'q': 59,
-    'r': 61,
-    's': 67,
-    't': 71,
-    'u': 73,
-    'v': 79,
-    'w': 83,
-    'x': 89,
-    'y': 97,
-    'z': 101
-    }
-
-
-def hash_func(word):
-  product = 1
-  for char in word:
-    product *= PRIMES[char]
-  return product
+from collections import defaultdict, Counter
 
 
 def anagrams(words):
@@ -47,7 +11,7 @@ def anagrams(words):
   """
   anagrams = defaultdict(list)
   for word in words:
-    anagrams[hash_func(word)].append(word)
+    anagrams[frozendict(Counter(word))].append(word)
   return anagrams.values()
 
 if __name__ == "__main__":
