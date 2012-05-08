707c7c296d057f04364f0d23a65d05c5be0f8254
Ceasar Bautista <cbautista2010@gmail.com>
2012-05-08 15:21:49 -0400

    Rewrote divide, splice, and flatten using itertools and added doctests.

--- listtools.py
+++ listtools.py
@@ -1,21 +1,31 @@
 """A collection of functions for manipulating iterables."""
-from itertools import izip
+from itertools import chain, izip, islice
 
 
-#NOTE: Generally itertools.chain is a better solution than either of the below
-flatten = lambda *x: sum(x, [])
-splice = lambda x, i, y: x[:i] + y + x[i:] 
+def flatten(*x):
+    """Flatten a list of lists.
+    >>> lists = [[1, 2], [3, 4], [5, 6]]
+    >>> list(flatten(*lists))
+    [1, 2, 3, 4, 5, 6]
+    """
+    return chain(*x)
+
+
+def splice(x, i, y):
+    """Splice one list into another.
+    >>> items = [1, 2, 5, 6]
+    >>> list(splice(items, 2, [3, 4]))
+    [1, 2, 3, 4, 5, 6]
+    """
+    return chain(islice(x, 0, i), y, islice(x, i, len(x)))
 
 
 def divide(list_, k):
-  '''Slice a list into pieces of length-k.
-
-  >>> items = range(12)
-  >>> for piece in divide(items, 4): print piece
-  [0, 1, 2, 3]
-  [4, 5, 6, 7]
-  [8, 9, 10, 11]
-  '''
+  """Slice a list into pieces of length-k.
+  >>> items = range(1, 7)
+  >>> list(divide(items, 2))
+  [[1, 2], [3, 4], [5, 6]]
+  """
   for i in range(0, len(list_), k):
     yield list_[i:i+k]
 
