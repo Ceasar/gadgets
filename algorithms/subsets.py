80d238ba5cf83afdc90067b2a1cdec693166ea5f
Ceasar Bautista <cbautista2010@gmail.com>
2016-07-18 15:32:27 -0700

    Delete subsets.py

--- algorithms/subsets.py
+++
@@ -1,9 +0,0 @@
-
-def subsets(container):
-  subsets = set()
-  for e in container:
-    x = frozenset(e)
-    subsets.add(x)
-    new_subsets = set(x | subset for subset in subsets)
-    subsets.update(new_subsets)
-  return subsets
